00229f58103dea97178127717655b8ee8544a53b
Patryk Kołodziejczyk <p.kolodziejczyk1996@gmail.com>
2021-07-22 21:44:37 +0200

    ADD: Command invokers

---
+++ src/enums/CommandInvoker.hpp
@@ -0,0 +1,10 @@
+#ifndef COMMANDINVOKER_HPP
+#define COMMANDINVOKER_HPP
+
+enum class CommandInvoker {
+  kLeftMouseButtonClick,
+  kRightMouseButtonClick,
+  kMiddleMouseButtonClick,
+};
+
+#endif //COMMANDINVOKER_HPP
