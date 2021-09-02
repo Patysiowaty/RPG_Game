9d6fdfb9c2228473520c7315369fb4ff3d724aed
Patryk Kołodziejczyk <p.kolodziejczyk1996@gmail.com>
2021-09-03 00:36:56 +0200

    ADD: render target member field in game option state

--- src/game_states/GameOptionsState.hpp
+++ src/game_states/GameOptionsState.hpp
@@ -7,11 +7,11 @@
 
 class GameOptionsState : public IGameState {
  public:
-  explicit GameOptionsState(GameStatesManager &game_states_manager);
+  explicit GameOptionsState(GameStatesManager &game_states_manager, sf::RenderWindow &render_window);
   void Initialize() override;
   void Render() override;
   void Update(float delta_time) override;
-  void Reload(const GameConfig& game_config) override;
+  void Reload(const GameConfig &game_config) override;
   void Cleanup() override;
   void Pause() override;
   void Resume() override;
@@ -19,6 +19,7 @@ class GameOptionsState : public IGameState {
  private:
   bool active_{true};
   GameStatesManager &game_states_manager_;
+  sf::RenderWindow &render_window_;
 
   sf::Font game_option_font_;
   Button return_button_;
