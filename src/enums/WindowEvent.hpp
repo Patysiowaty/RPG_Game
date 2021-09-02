b5051d8177661eccd990cd929b00502dcc226e0f
Patryk Kołodziejczyk <p.kolodziejczyk1996@gmail.com>
2021-09-03 00:22:32 +0200

    ADD: Game windows events enum

---
+++ src/enums/WindowEvent.hpp
@@ -0,0 +1,6 @@
+#ifndef WINDOWEVENT_HPP
+#define WINDOWEVENT_HPP
+enum class WindowEvent {
+  kLeftMouseClick, kRightMouseClick, kActivateWindow, kDeactivateWindow, kRestoreDefault
+};
+#endif //WINDOWEVENT_HPP
