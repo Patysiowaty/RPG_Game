7125ff28462441b056599329202fff9f70602b37
Patryk Kołodziejczyk <p.kolodziejczyk1996@gmail.com>
2021-09-03 00:38:51 +0200

    CHG: capacity of player inventory

--- src/game_core/PlayerInventory.hpp
+++ src/game_core/PlayerInventory.hpp
@@ -2,15 +2,17 @@
 #define PLAYERINVENTORY_HPP
 
 #include <memory>
+#include <map>
 #include <vector>
 #include "../item_types/Item.hpp"
 #include "../enums/ErrorCodes/ItemInteractorErrorCode.hpp"
 #include "../interfaces/IItemList.hpp"
+#include "../SlotIndex.hpp"
 
 class PlayerInventory : IItemList {
  public:
   explicit PlayerInventory(std::uint16_t available_tabs = 1);
-  ~PlayerInventory();
+  ~PlayerInventory() = default;
 
   void SetInventoryAvailableTabs(std::uint16_t value);
 
@@ -18,6 +20,8 @@ class PlayerInventory : IItemList {
   bool RemoveItem(std::shared_ptr<Item> item) override;
   bool PutItem(std::shared_ptr<Item> item, uint16_t position);
   bool PutItem(std::shared_ptr<Item> item) override;
+
+  const std::vector<std::shared_ptr<Item>> &GetInventory() const { return inventory_; }
 
   std::shared_ptr<Item> GetItem(std::uint32_t item_id) const;
 
@@ -29,12 +33,14 @@ class PlayerInventory : IItemList {
   bool IsAvailableTab(std::uint16_t position) const;
 
   std::uint16_t GetInventoryAvailableTabs() const;
+  static std::uint16_t GetTabCapacity();
+  static std::uint16_t GetMaxTabs();
 
  private:
-  static constexpr std::uint16_t kInventoryTabCapacity = 20;
-  static constexpr std::uint16_t kInventoryMaxTabs = 4;
-  std::uint16_t inventory_available_tabs_ = 1;
-  std::uint16_t inventory_space_;
+  static constexpr std::uint16_t kTabCapacity{40};
+  static constexpr std::uint16_t kMaxTabs{4};
+  std::uint16_t available_tabs_{1};
+  std::uint16_t available_space_;
   std::uint16_t remaining_slots_;
 
   std::vector<std::shared_ptr<Item>> inventory_;
