4eb5e164ec1b6463e1fcdc780979593a003e4b28
Patryk Kołodziejczyk <p.kolodziejczyk1996@gmail.com>
2021-09-05 22:48:39 +0200

    ADD: main error log

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "game_core/Game.hpp"
+#include <iostream>
 
-int main() {
+int main() try {
   Game game;
   game.InitializeResources();
   game.Run();
+
+  return EXIT_SUCCESS;
+} catch (std::exception &e) {
+  std::cerr << "An error has occurred: " << e.what() << "\n";
+  return EXIT_FAILURE;
 }
