079edff91a08c1fe83a88ae526bb22dce42f2cdb
Patryk Kołodziejczyk <p.kolodziejczyk1996@gmail.com>
2021-09-05 22:40:36 +0200

    ADD: Getter of window size property

--- src/interfaces/IGameWindow.hpp
+++ src/interfaces/IGameWindow.hpp
@@ -1,12 +1,16 @@
 #ifndef IGAMEWINDOW_HPP
 #define IGAMEWINDOW_HPP
 
+#include <SFML/System/Vector2.hpp>
+
 class IGameWindow {
  public:
   ~IGameWindow() = default;
   virtual void OpenWindow() = 0;
   virtual void CloseWindow() = 0;
   virtual bool IsOpen() const = 0;
+  virtual void Move(const sf::Vector2f &offset) = 0;
+  virtual const sf::Vector2f &GetWindowSize() const = 0;
 };
 
 #endif //IGAMEWINDOW_HPP
