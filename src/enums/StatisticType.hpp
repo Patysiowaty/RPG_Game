9dcd5f83dacbc2f54f11dbd1376c9d9c42c6131d
Patryk Kołodziejczyk <p.kolodziejczyk1996@gmail.com>
2021-10-06 22:58:30 +0200

    ADD: size of StatisticType enum

--- src/enums/StatisticType.hpp
+++ src/enums/StatisticType.hpp
@@ -2,17 +2,19 @@
 #define STATISTICTYPE_HPP
 
 enum class StatisticType {
-  kNone = 0,
-  kAttack,
-  kMagicAttack,
-  kHealth,
-  kStamina,
-  kMana,
-  kArmor,
-  kMagicResistance,
-  kEvadeRatio,
-  kCriticalStrikeRatio,
-  kCriticalStrikeForce
+  kNone = -1,
+  kAttack = 0,
+  kMagicAttack = 1,
+  kHealth = 2,
+  kArmor = 3,
+  kStamina = 4,
+  kMana = 5,
+  kMagicResistance = 6,
+  kEvadeRatio = 7,
+  kCriticalStrikeRatio = 8,
+  kCriticalStrikeForce = 9,
+
+  kSize = 10,
 };
 
 #endif //STATISTICTYPE_HPP
